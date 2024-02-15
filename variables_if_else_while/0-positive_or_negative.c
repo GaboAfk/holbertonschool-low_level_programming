1510de1a126a2186de695e8a1c8f76732c67614b
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-15 13:28:06 -0300

    ??

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,28 @@
 /*
  * File: 0-positive_or_negative.c
- * Auth: Gabriel Morffe <8677@holbertonschool.com>
+ * Auth: Gabriel Morffe
  */
 
-#include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- * randomize n, know is value
- * Return: Always 0 (Success)
- */
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
 int main(void)
 {
 	int n;
-	
-	srand(time(0));
-	n = rand () - RAND_MAX / 2;
 
-	if (n>0)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n<0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
