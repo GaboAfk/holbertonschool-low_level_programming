1a190e407b5ef1676926301a0a3a775468cc66c2
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-15 11:06:21 -0300

    printfff

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,17 @@
+/*
+ * File: 5-printf.c
+ * Auth: Gabriel Morffe <8677@holbertonschool.com>
+ */
+
 #include <stdio.h>
+
 /**
  * main - Entry point
  * printf frase
  * Return: Always 0 (Success)
  */
+
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
