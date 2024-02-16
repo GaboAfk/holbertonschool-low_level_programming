de05617905d338f02db3257a144bd832bf971a51
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-16 13:34:08 -0300

    2 2

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			for (k = i; k <= 9; k++)
+			{
+				for (l = j; l <= 9; l++)
+				{
+					if (k == i && j == l)
+						l++;
+
+					putchar(i + '0');
+					putchar(j + '0');
+
+					putchar(' ');
+
+					putchar(k + '0');
+					putchar(l + '0');
+
+					if (i == 9 && j == 8)
+					break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
