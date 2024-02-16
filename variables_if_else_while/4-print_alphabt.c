41783829041b4051f663661599f274d6f715fd17
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-16 10:27:34 -0300

    alphabet except e and q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase except 'q' and 'e'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
