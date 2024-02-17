86e46688e903bc095b45eb77972fc3e0cd25c7db
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-17 20:50:00 -0300

    $commit

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,8 +2,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase, followed 
- * 			by a new line
+ * print_alphabet - prints the alphabet in lowercase, followed
+ *			by a new line
  *
  * Return: void
  */
@@ -11,8 +11,8 @@ void print_alphabet(void)
 {
 	int i;
 
-	for(i = 97; i < 123; i++)
+	for (i = 97; i < 123; i++)
 		_putchar(i);
-		
+
 	_putchar('\n');
 }
