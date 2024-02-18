3570852283a33227f80978e6375fbb21e338f6c7
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-17 21:00:19 -0300

    $commit

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,14 +2,20 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - use print_alphabet 10 times
+ * print_alphabet - prints the alphabet in lowercase, followed
+ *			by a new line
  *
  * Return: void
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
-	int i;
+	int i, j;
+	
+	for (j = 0; i < 10; j++)
+	{
+		for (i = 97; i < 123; i++)
+			_putchar(i);
 
-	for (i = 0; i < 10; i++)
-		print_alphabet();
+		_putchar('\n');
+	}
 }
