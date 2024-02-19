1b1b9ddc32916cc93e5543298be1cc99c4d76098
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-19 17:10:51 -0300

    tabla 15?

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ *
+ * Return: NULL
+ */
+void print_times_table(int n)
+{
+        int i, j, x;
+
+	if (n > 0 || n < 15)
+	{
+        	for (i = 0; i <= n; i++)
+        	{
+			for (j = 0; j <= n; j++)
+                	{
+                        	x = i * j;
+
+				if (x <= 9)
+			   		_putchar(x + '0');
+	
+				else if (x <= 99)
+                        	{
+                                	_putchar(x / 10 + '0');
+                                	_putchar(x % 10 + '0');
+                       	 	}	
+				else
+                        	{
+                                	_putchar(x / 100 + '0');
+                                	_putchar((x / 10) % 10 + '0');
+                                	_putchar(x % 10 + '0');
+                       	 	}
+
+
+				if (j < n)
+                        	{
+                                	_putchar(',');
+                                	_putchar(' ');
+
+                                	if (x < 100)
+					{
+                                        	_putchar(' ');
+
+						if (x < 10) 
+                                        		_putchar(' ');
+					}
+                        	}
+                	}
+	        _putchar('\n');
+       		}
+	}
+}
