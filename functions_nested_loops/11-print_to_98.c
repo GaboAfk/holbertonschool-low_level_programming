d4a0f59fa00e118d6c39efe9c050fdd043e22668
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-19 15:17:56 -0300

    print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from @n to 98, followed by a new line.
+ * @n: first number to star counting.
+ *
+ * Return: NULL
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		int i;
+		for (i = n; i < 98; i++)
+			printf("%d, ",i);
+	}
+	else if (n > 98)
+	{
+		int i;
+		for (i = n; i > 98; i--)
+			printf("%d, ",i);
+	}
+	else	printf("98\n");
+}
