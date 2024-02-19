745b9fcbc11880a2a7328c118eca95d52344471a
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-19 14:42:13 -0300

    ' '

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -17,18 +17,20 @@ void times_table(void)
 			n = i * j;
 
 			if (n <= 9)
-			{
 				_putchar(n + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
 			else
 			{
 				_putchar(n / 10 + '0');
 				_putchar(n % 10 + '0');
+			}
+
+			if (j < 9)
+			{
 				_putchar(',');
 				_putchar(' ');
+			
+				if (n + j <= 9)
+					_putchar(' ');
 			}
 		}
 	_putchar('\n');
