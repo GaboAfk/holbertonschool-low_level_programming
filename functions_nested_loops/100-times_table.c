693f70ec47b07e8f249e25ce96f2db5e57f27268
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-19 17:51:23 -0300

    bettyplzz

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -7,18 +7,18 @@
  */
 void print_times_table(int n)
 {
-        int i, j, x;
+	int i, j, x;
 
 	if (n == 0 || (n > 0 && n < 15))
 	{
-        	for (i = 0; i <= n; i++)
-        	{
+		for (i = 0; i <= n; i++)
+		{
 			for (j = 0; j <= n; j++)
-                	{
-                        	x = i * j;
+			{
+				x = i * j;
 
 				if (x <= 9)
-			   		_putchar(x + '0');
+					_putchar(x + '0');
 	
 				else if (x <= 99)
                         	{
