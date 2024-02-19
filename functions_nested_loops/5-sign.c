2796ad8be967b1d4279a638190caaef4fce8a4e9
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-19 11:27:30 -0300

    print_sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - prints the sing of @n.
+ * @c: number in fact.
+ * Return: 0, 1 or -1.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if else (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else return (0);
+}
