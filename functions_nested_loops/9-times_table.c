12da702d647006cc5008746965e6c6648de5a134
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-19 14:46:07 -0300

    betty plz

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	int i, j, n;
 
 	for (i = 0; i <= 9; i++)
-	{	
+	{
 		for (j = 0; j <= 9; j++)
 		{
 			n = i * j;
@@ -28,7 +28,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-			
+	
 				if (n + i <= 9)
 					_putchar(' ');
 			}
