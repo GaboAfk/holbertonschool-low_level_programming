eda8191c9733a94995ef868c917e964ebae8238f
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-19 12:17:41 -0300

    %10

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of @n.
+ * @n: number in fact.
+ *
+ * Return: 0.
+ */
+int _print_last_digit(int n)
+{
+	return (n % 10);
+}
