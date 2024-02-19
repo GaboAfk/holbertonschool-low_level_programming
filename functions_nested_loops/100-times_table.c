6385691a8468e01e307d2473ebab52875cecc60e
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-19 18:11:48 -0300

    bettyplzz final

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -14,23 +14,19 @@ void print_times_table(int n)
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
-			{
-				x = i * j;
-
+			{	x = i * j;
 				if (x <= 9)
 					_putchar(x + '0');
-
 				else if (x <= 99)
-				{
-					_putchar(x / 10 + '0');
+				{	_putchar(x / 10 + '0');
 					_putchar(x % 10 + '0');
 				}
 				else
 				{
-					_putchar(x / 100 + '0'), putchar(((x / 10) % 10) + '0');
+					_putchar(x / 100 + '0');
+					_putchar(((x / 10) % 10) + '0');
 					_putchar(x % 10 + '0');
 				}
-
 				if (j < n)
 				{
 					_putchar(',');
@@ -38,10 +34,9 @@ void print_times_table(int n)
 
 					if (x + i < 100)
 					{
+					_putchar(' ');
+					if (x + i < 10)
 						_putchar(' ');
-
-						if (x + i < 10)
-							_putchar(' ');
 					}
 				}
 			}
