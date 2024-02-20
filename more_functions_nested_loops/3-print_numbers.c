c6976160772ef333fd7629e5ca324082a27df4b7
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-20 02:51:54 -0300

    betty

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9 followed by a new line using only two _putchar
+ * print_numbers - prints the numbers, from 0 to 9
+ * 		 followed by a new line using only two _putchar
  *
  */
 void print_numbers(void)
 {
 	int i;
 
-	for(i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i + '0');
 	}
