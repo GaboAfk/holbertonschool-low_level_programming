2612a07cdc2130952e90062faa9beba2a8b888cd
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-20 03:23:58 -0300

    printlines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line depending of @n.
+ * @n: number of lines to draw.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+}
