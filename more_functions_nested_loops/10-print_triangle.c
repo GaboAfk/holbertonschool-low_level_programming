f3bc58ddb74dd5d850f9b48995bb7bfddee2b83e
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-20 07:52:06 -0300

    ahorasi

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,9 +10,9 @@ void print_triangle(int size)
 
 	for (i = 0; i < size; i++)
 	{	
-		for (j = 0; j < size; j++)
+		for (j = 0; j < i; j++)
 		{
-			for (k = 0; k < j; k++)
+			for (k = size - 1; k < 0; k--)
 				_putchar(' ');
 			_putchar('#');
 		}
@@ -22,3 +22,5 @@ void print_triangle(int size)
 	if (size <= 0)
 		_putchar('\n');
 }
+			
+
