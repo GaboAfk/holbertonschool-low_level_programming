f3fe9f50a1b5fdc021329fa06db789ad0628b792
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-20 05:33:16 -0300

    for 1 - 100

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0)
 			printf("Fizz");
@@ -23,7 +23,7 @@ int main(void)
 
 		else printf("%d",i);
 
-		if (i < 100)
+		if (i <= 100)
 			printf(" ");
 	}
 	printf("\n");
