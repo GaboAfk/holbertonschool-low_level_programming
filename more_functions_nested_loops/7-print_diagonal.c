2bfc3b2fcfa46b14e5b762a965d4ca55bcf0ced5
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-20 03:53:10 -0300

    if

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,7 +12,10 @@ void print_diagonal(int n)
 	{
 		for (j = 0; j < i; j++)
 			_putchar(' ');
-		_putchar(47);
+		_putchar(92);
+		_putchar('\n');
 	}
-	_putchar('\n');
+
+	if (n <= 0)
+		_putchar('\n');
 }
