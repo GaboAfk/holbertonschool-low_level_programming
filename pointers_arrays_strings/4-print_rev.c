2d43c48c538788df99becb00e643079a04c0d50f
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-21 13:29:31 -0300

    string reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+/*
+ * File: 4-print_rev.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new
+ *		line, to stdout.
+ * @s: string.
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+		j++;
+
+	for (i = j - 1; i > 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
