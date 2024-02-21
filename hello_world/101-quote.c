a26d98fc47c411b7979505eef3d58f2897dcc08e
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-20 22:59:36 -0300

    " - Dora Korpar

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>  // Para la función write
+#include <string.h>  // Para la función strlen
+
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, strlen(message));
+
+	return 1;
+}
