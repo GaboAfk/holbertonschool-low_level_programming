b8fe024e57b3d378bbdbdc65a8c2fabee38316e9
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-21 12:11:09 -0300

    402 to 98 bettyupdate

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98.
+ * reset_to_98 - takes a pointer @*n to an int and updates the value to 98.
+ * @n: pointer.
  */
 void reset_to_98(int *n)
 {
