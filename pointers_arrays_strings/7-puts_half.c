bd79d32b48e87c799be0b6a5835c1e0ce8fac1ad
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-21 15:28:03 -0300

    half string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+/*
+ * File: 7-puts_half.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string.
+ * @str: string.
+ * Return: Always 0 (Success)
+ */
+void puts_half(char *str)
+{
+	int i, j = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		j++;
+
+	if (j % 2 == 1)
+		j++;
+
+	for (i = j / 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+               _putchar('\n');
+}
