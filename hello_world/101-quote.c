adc2a3494e9e222fb96018c10fa21f149a60eb56
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-20 23:39:26 -0300

    stopbetty

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-	char message[] = 
+	char message[] =
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	int i;
 
-	for (i = 0; message[i] != '\0'; i++)	
+	for (i = 0; message[i] != '\0'; i++)
 		write(2, &message[i], 1);
 
 	return (1);
