12932eaaf5c649a15e5ed575d9b706e6a3107d43
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-20 23:38:22 -0300

    is betty here too?!

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,17 @@
 #include <unistd.h>
-
+/**
+ * main - Prints out message in the standard error.
+ * Return: 1 if success.
+ */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char message[] = 
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	int i;
 
 	for (i = 0; message[i] != '\0'; i++)	
 		write(2, &message[i], 1);
 
-	return 1;
+	return (1);
 }
