6595d1b3ad4aac709523761af461bf1c7ded8cbd
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-23 10:36:35 -0300

    _strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-strcat.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings.
+ * @dest: result string.
+ * @src: string to appends.
+ *
+ * Return: @dest modify.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *result = dest;
+
+	while (*dest != '\0')
+		dest++;
+
+	while (*src != '\0')
+		*dest++ = *src++;
+	*dest++ = '\0';
+
+	return (result);
+}
