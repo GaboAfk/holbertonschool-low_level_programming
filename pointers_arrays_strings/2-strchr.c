6f19675063b3409b466b4ea6217f5f786d160058
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-26 17:56:00 -0300

    strchr function

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+/*
+ * File: 2-trchr.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string.
+ * @c: char to find.
+ *
+ * Return: pointer to the first ocurrence or NULL if dont.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
