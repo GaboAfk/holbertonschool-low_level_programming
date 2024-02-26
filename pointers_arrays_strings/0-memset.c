31d116466fcbaf7cec4596825c5803c4a8ad1aa6
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-26 17:26:11 -0300

    menset function

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+/*
+ * File: 0-memset.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte.
+ * @s: string to modify.
+ * @b: char to use in b.
+ * @n: first n bytes to modify.
+ *
+ * Return: @s modified.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
