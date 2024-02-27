d8d96742d175c5a8b3369e71a1534f60dc339315
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-27 15:29:55 -0300

    print reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+/*
+ * File: 1-print_rev_recursion.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: string.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
