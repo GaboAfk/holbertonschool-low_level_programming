caf9eb7ee85f4a905e18d1d3f41a3b3242f488de
GaboAfk <gabrielmorffe95@gmail.com>
2024-02-29 11:53:47 -0300

    sizeof in malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -21,8 +21,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	a = malloc(size);
-
+	a = malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 	{
 		a[i] = c;
