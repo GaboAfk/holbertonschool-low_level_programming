af17e35e84fbf8a2b1c7fbcb9a1c87fbc70da7ed
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-01 12:39:00 -0300

    dinamic grid of 0

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+/*
+ * File: 3-alloc_grid.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the grid.
+ * @height: height of the grid.
+ * Return: a pointer to a 2 dimensional array. NULL if width or height is
+ *				negative or 0. NULL if dont have enoght memory.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(width * sizeof(int *));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < width; i++)
+	{
+		grid[i] = malloc(height * sizeof(int));
+
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+
+		for (j = 0; j < height; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
+}
