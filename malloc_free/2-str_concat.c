79cb95614e9cbfa2f66687b567b3a2a47a8458f3
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-01 11:26:29 -0300

    gcc error fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,5 +1,5 @@
 /*
- * File: 1-strdup.c
+ * File: 2str_concat.c
  * Auth: Gabriel Morffe
  */
 
@@ -7,35 +7,72 @@
 #include <stdlib.h>
 
 /**
- * *_strdup - using malloc for allocated space, which contains a copy of
- *				the string given as a parameter.
- * @str: string to duplicate.
- * Return: pointer to a copy of @str. NULL if str = NULL or insufficient
- *				memor was available.
+ * *str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * Return: pointer to a newly allocated space in memory with s1 followed by s2.
  */
-char *_strdup(char *str)
-{
-	char *a;
-	int i, size = 0;
+char *str_concat(char *s1, char *s2)
+{{
+	char *res;
+	int i, size1 = 0, size2 = 0;
+	int e1 = 0, e2 = 0;
 
-	if (!str)
-		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
+	if (s1)
 	{
-		size++;
+		e1 = 1;
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			size1++;
+		}
 	}
 
-	a = malloc((size + 1) * sizeof(*str));
-
-	if (a != NULL)
+	if (s2)
 	{
-		for (i = 0; i < size; i++)
+		e2 = 1;
+		for (i = 0; s2[i] != '\0'; i++)
 		{
-			a[i] = str[i];
+			size2++;
+		}
+	}
+
+	if (e1 && e2)
+		res = malloc((size1 + size2 + 1) * sizeof(char));
+	else
+		res = malloc((e1 * (size1 + 1) + e2 * (size2 + 1)) * sizeof(char));
+
+	if (res != NULL)
+	{
+		if (e1 && e2)
+		{
+			for (i = 0; i < size1; i++)
+			{
+				res[i] = s1[i];
+			}
+			for (; i < size1 + size2; i++)
+			{
+				res[i] = s2[i - size1];
+			}
+			res[i] = '\0';
+		}
+		else if (e1)
+		{
+			for (i = 0; i < size1; i++)
+			{
+				res[i] = s1[i];
+			}
+			res[i] = '\0';
+		}
+		else
+		{
+			for (i = 0; i < size2; i++)
+			{
+				res[i] = s2[i];
+			}
+			res[i] = '\0';
 		}
 	}
 	else
 		return (NULL);
-	return (a);
-}
+	return (res);
+}}
