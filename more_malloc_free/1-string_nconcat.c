fd2e0ddb2d7609b78198ec8ac094942e5165d946
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-04 12:29:23 -0300

    malloc string n concat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+/*
+ * File: 1string_nconcat.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - allocates memory using malloc.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: number of bytes to copy.
+ * Return: pointer to a new allocated with @s1 followed
+ *			by n bytes of @s2.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p = malloc(sizeof(s1) + n + 1);
+	unsigned int i = 0, j;
+
+	if (!p)
+		return (NULL);
+
+	while (s1[i] != '\0')
+	{
+		p[i] = s1[i];
+		i++;
+	}
+
+	for (j = 0; j < n && s2[j] != '\0'; i++, j++)
+		p[i] = s2[j];
+
+	return (p);
+}
