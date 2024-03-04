bb87fa1de9cab65e315eedfd3369d1c937420e48
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-04 13:28:59 -0300

    fix return

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -5,6 +5,7 @@
 
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * *string_nconcat - allocates memory using malloc.
@@ -16,22 +17,23 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p = malloc(sizeof(s1) + n + 1);
+	char *p;
 	unsigned int i = 0, j;
 
+	while (s1[i] != '\0')
+		i++;
+
+	p = malloc(sizeof(char) * i + n + 1);
 	if (!p)
 		return (NULL);
 
-	while (s1[i] != '\0')
-	{
+	for (i = 0; s1[i] != '\0'; i++)
 		p[i] = s1[i];
-		i++;
-	}
 
 	for (j = 0; j < n && s2[j] != '\0'; i++, j++)
 		p[i] = s2[j];
 
-	p[j + 1] = '\0';
+	p[i] = '\0';
 
 	return (p);
 }
