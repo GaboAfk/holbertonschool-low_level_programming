943f2345eed4cadf98886d68121a9ba44e19e34e
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-05 12:46:40 -0300

    memory fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -5,7 +5,37 @@
 
 #include "dog.h"
 #include <stdlib.h>
+/**
+ * *_strdup - using malloc for allocated space, which contains a copy of
+ *				the string given as a parameter.
+ * @str: string to duplicate.
+ * Return: pointer to a copy of @str. NULL if str = NULL or insufficient
+ *				memor was available.
+ */
+char *_strdup(char *str)
+{
+	char *a;
+	int i, size = 0;
 
+	if (!str)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		size++;
+	}
+
+	a = malloc((size + 1) * sizeof(*str));
+
+	if (a != NULL)
+	{
+		for (i = 0; i < size; i++)
+			a[i] = str[i];
+	}
+	else
+		return (NULL);
+	return (a);
+}
 /**
  * *new_dog - function that creates a new dog.
  * @name: name of the dog.
@@ -28,9 +58,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new->name = name;
+	new->name = _strdup(name);
 	new->age = age;
-	new->owner = owner;
+	new->owner = _strdup(owner);
 
 	return (new);
 }
