76199c9ea82bb2fbc95e60aaa6127e6a68f62fb8
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-05 10:53:05 -0300

    fix error

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -18,11 +18,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	if (d == NULL)
 		return;
 
-	struct dog new;
-
-	new.name = name;
-	new.age = age;
-	new.owner = owner;
-
-	*d = new;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
