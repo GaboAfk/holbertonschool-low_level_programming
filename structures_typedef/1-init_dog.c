22b7bdbb628e87666713b38f4375303dc52f0bc0
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-05 10:45:57 -0300

    init struct

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+/*
+ * File: 1-init_dog.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "dog.h"
+
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ * @d: pointer to dog structure.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog new;
+
+	new.name = name;
+	new.age = age;
+	new.owner = owner;
+
+	*d = new;
+}
---
+++ structures_typedef/dogf.h
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo 'prototype?'
+read f
+echo "$f" >> "dog.h"
