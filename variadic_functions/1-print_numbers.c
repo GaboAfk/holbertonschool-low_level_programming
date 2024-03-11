d8b3a6bc52a3de27bac9477f40612aa33553ee4c
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-11 10:57:56 -0300

    variadic print numb

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+/*
+ * File: 1-print_numbers.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - function that prints numbers, followed by a new line.
+ * @separator: string between numbers.
+ * @n: integers passed.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i;
+
+	va_start(nums, n);
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d", va_arg(nums, int));
+		if (separator)
+			printf("%s", separator);
+	}
+
+	printf("%d", va_arg(nums, int));
+	printf("\n");
+
+}
