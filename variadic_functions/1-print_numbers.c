69027e3d84d183014af285aaa47ae4ace0ad491f
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-11 11:02:31 -0300

    fix n NULL

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,19 +13,21 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int i;
-
-	va_start(nums, n);
-
-	for (i = 0; i < n - 1; i++)
+	if (n)
 	{
+		va_list nums;
+		unsigned int i;
+
+		va_start(nums, n);
+
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d", va_arg(nums, int));
+			if (separator)
+				printf("%s", separator);
+		}
+
 		printf("%d", va_arg(nums, int));
-		if (separator)
-			printf("%s", separator);
+		printf("\n");
 	}
-
-	printf("%d", va_arg(nums, int));
-	printf("\n");
-
 }
