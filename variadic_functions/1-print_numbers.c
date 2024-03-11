4cdfabf9b63ae48783db927972782496968773be
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-11 11:15:49 -0300

    fix printf space

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,11 +13,13 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-		va_list nums;
-		unsigned int i;
+	va_list nums;
+	unsigned int i;
 
-		va_start(nums, n);
+	va_start(nums, n);
 
+	if (n > 0)
+	{
 		for (i = 0; i < n - 1; i++)
 		{
 			printf("%d", va_arg(nums, int));
@@ -25,7 +27,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 				printf("%s", separator);
 		}
 
-	if (n > 0)
 		printf("%d", va_arg(nums, int));
-	printf("\n");
+	}
+		printf("\n");
 }
