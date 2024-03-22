54fec7df57e0d5a66fe783b1d9b687d9d0c0a7e1
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-22 12:45:57 -0300

    fix strlen prohibited

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -15,6 +15,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
+	unsigned int i;
 
 	if (!new)
 		return (NULL);
@@ -25,7 +26,10 @@ list_t *add_node(list_t **head, const char *str)
 		free(new);
 		return (NULL);
 	}
-	new->len = strlen(str);
+
+	while (new->str[i])
+		i++;
+	new->len = i;
 	new->next = *head;
 
 	*head = new;
