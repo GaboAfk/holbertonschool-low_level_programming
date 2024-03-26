8427f066baf5cdd3ac3837391b78601015698740
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-25 21:20:34 -0300

    print double linked list function

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-print_dlistint.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - function that prints all the elements of a dlistint_t list.
+ * @h: double list linked.
+ * Return: the number of nodes.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (count);
+}
