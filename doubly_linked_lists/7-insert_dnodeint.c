bc2a41b051aad65c88b5c97b940d1ceb3213b890
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-27 14:55:15 -0300

    insert in pos function / header update

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+/*
+ * File: 7-insert_dnodeint.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - function that inserts a
+ *								new node at a given position.
+ * @h: pointer to double linked list pointer.
+ * @idx: index of the list where the new node should be added. Start at 0.
+ * @n: number to add.
+ * Return: address of the new list. NULL if fails or is not possible to add
+ *				the new node at index at @idx.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *aux = NULL, *new = *h;
+	unsigned int pos = 0;
+
+	if (idx == 0) /*idx 0 agregar al inicio*/
+		return (add_dnodeint(&(*h), n));
+
+	while (new && pos < idx)
+	{
+		new = new->next;
+		pos++;
+	}
+
+	if (!new && pos == idx) /* agregar nodo al final */
+	{
+		add_dnodeint_end(&new, n);
+		return (*h);
+	}
+
+	if (pos != idx) /*no hay suficientes nodos*/
+		return (NULL);
+
+	aux = new->prev;
+
+	add_dnodeint(&new, n);
+
+	if (aux)
+		aux->next = new;
+
+	new->prev = aux;
+
+	return (*h);
+}
