2a05815a5aea4edd172415961f4dfa8f87ef8cd1
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-28 20:41:15 -0300

    delete function / header update

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,42 @@
+/*
+ * File: 8-delete_dnodeint.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index index of a
+ *								dlistint_t linked list.
+ * @head: pointer to a double linked list pointer.
+ * @index: index of the node that should be deleted, start at 0.
+ * Return: 1 (Success). -1 (Fail).
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *aux = *head;
+	unsigned int pos = 0;
+
+	if (!head)/*lista vacía*/
+		return (-1);
+
+	while (aux && pos < index)
+	{
+		aux = aux->next;
+		pos++;
+	}
+
+	if (!aux)/*llegué al final*/
+		return (-1);
+
+	if (aux->prev)
+		aux->prev->next = aux->next;
+	else
+		*head = (*head)->next;
+
+	if (aux->next)
+		aux->next->prev = aux->prev;
+	free(aux);
+	aux = NULL;
+	return (1);
+}
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LIST
 #define LIST
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -26,4 +26,5 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 #endif
