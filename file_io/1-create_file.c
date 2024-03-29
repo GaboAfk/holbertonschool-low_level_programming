4d27709a339001ab38ab9a8f4da03037dbc74c5c
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-29 18:26:22 -0300

    write fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	if (!buf)
 		buf = "";
 
-	w =	write(fd, buf, strlen(buf) + 1);
+	w =	write(fd, buf, strlen(buf));
 	if (w == -1)
 		return (-1);
 
