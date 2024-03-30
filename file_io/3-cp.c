02086fbb012b9d84f2c2c3732ff5b96e61af839a
GaboAfk <gabrielmorffe95@gmail.com>
2024-03-30 17:47:49 -0300

    cp function

---
+++ file_io/3-cp.c
@@ -0,0 +1,57 @@
+/*
+ * File: 3-cp.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that copies the content of a file to another file.
+ * @ac: number of arguments.
+ * @av: arguments.
+ * Return: Always 0 (Success). 97 (incorrect @ac).
+ */
+int main(int ac, char **av)
+{
+	int file_to, file_from, bytes_read;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	file_from = open(av[1], O_RDONLY);
+
+	bytes_read = read(file_from, buffer, 1024);
+	if (bytes_read == -1 || file_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	while (bytes_read > 0 && file_from > 0)
+	{
+		if (write(file_to, buffer, strlen(buffer)) < 0)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+		bytes_read = read(file_from, buffer, 1024);
+	}
+
+	if (close(file_from) == -1)
+	{
+		dprintf(2, "Error: Can't iclose fd %d\n", file_from);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(2, "Error: Can't iclose fd %d\n", file_to);
+		exit(100);
+	}
+	return (0);
+}
